cfa32f65a095e2096cf905a66d02c359d881de5a
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-04 20:44:02 +0300

    feat: add AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@
 import './Main.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   return (
     <>
       <Header />
       <Promo />
+      <AboutProject />
     </>    
   )
 }
