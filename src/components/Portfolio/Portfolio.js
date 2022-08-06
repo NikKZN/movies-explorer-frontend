b4e4e5e45b9d7b5d1199a0e9b92518655688bd68
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-06 07:56:23 +0300

    feat: add Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,34 @@
-import './Portfolio.css';
+import "./Portfolio.css";
+import arrow from "../../images/AboutMe/arrow.svg";
 
 function Portfolio() {
   return (
     <>
-      <p>Портфолио</p>
+      <div className="main__portfolio portfolio">
+        <h2 className="portfolio__title">Портфолио</h2>
+        <ul className="portfolio__links">
+          <li className="portfolio__item">
+            <a className="portfolio__link" href="https://github.com/NikKZN">
+              Статичный сайт
+              <img className="portfolio__link-img" src={arrow} alt="Стрелка." />
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a className="portfolio__link" href="https://github.com/NikKZN">
+              Адаптивный сайт
+              <img className="portfolio__link-img" src={arrow} alt="Стрелка." />
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a className="portfolio__link" href="https://github.com/NikKZN">
+              Одностраничное приложение
+              <img className="portfolio__link-img" src={arrow} alt="Стрелка." />
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
 export default Portfolio;
