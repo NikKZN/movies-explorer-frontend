c9e4dfb205df053fbc3e485b42c45efd70deb56f
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-09 07:39:47 +0300

    feat: add Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,45 @@
-import './Profile.css';
+import Header from "../Header/Header";
+import "./Profile.css";
 
 function Profile() {
   return (
-      <p className='p4'>Тут профиль</p>
+    <>
+      <Header />
+      <section className="profile">
+        <h1 className="profile__greeting">Привет, Виталий!</h1>
+        <form className="profile__edit-form">
+          <label className="profile__label">
+            Имя
+            <input
+              className="profile__input"
+              name="name"
+              type="text"
+              placeholder="Виталий"
+              required
+            />
+          </label>
+          <label className="profile__label">
+            E-mail
+            <input
+              className="profile__input"
+              name="email"
+              type="email"
+              placeholder="pochta@yandex.ru"
+              required
+            />
+          </label>
+        </form>
+        <div className="profile__buttons">
+          <button className="profile__button-edit" type="submit">
+            Редактировать
+          </button>
+          <button className="profile__button-exit" type="button" role="link">
+            Выйти из аккаунта
+          </button>
+        </div>
+      </section>
+    </>
   );
-};
+}
 
 export default Profile;
