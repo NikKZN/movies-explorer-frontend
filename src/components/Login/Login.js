c33d33fd629101e29ab6fdda42e4252a65b0bff4
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-11 18:51:39 +0300

    feat: add Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,53 @@
-import './Login.css';
+import "./Login.css";
+import logo from "../../images/Header/header-logo.svg";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-      <p className='p5'>Тут страница входа</p>
+    <>
+      <section className="login">
+        <div className="login__content">
+          <img className="login__logo" src={logo} alt="Логотип фильмотека." />
+          <h1 className="login__greeting">Рады видеть!</h1>
+          <form className="login__form">
+            <label className="login__label">
+              E-mail
+              <input
+                className="login__input"
+                name="email"
+                type="email"
+                placeholder="pochta@yandex.ru|"
+                required
+              />
+              <span className="login__input-error">Что-то пошло не так...</span>
+            </label>
+            <label className="login__label">
+              Пароль
+              <input
+                className="login__input"
+                name="password"
+                type="password"
+                placeholder="Пароль"
+                minLength="8"
+                autoComplete="off"
+                required
+              />
+              <span className="login__input-error">Что-то пошло не так...</span>
+            </label>
+            <button className="login__submit-button" type="submit">
+              Войти
+            </button>
+            <p className="login__caption">
+              Ещё не зарегистрированы?
+              <Link to="/signup" className="login__caption-link">
+                Регистрация
+              </Link>
+            </p>
+          </form>
+        </div>
+      </section>
+    </>
   );
-};
+}
 
 export default Login;
