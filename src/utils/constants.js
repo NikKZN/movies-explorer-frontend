f0b0dc734a9a08dc66ee0ab5aaf32105a0ceb44b
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-23 21:00:43 +0300

    feat: add requests mainApi

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 const MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
 
-const BASE_URL = "https://api.filmoteka.nkzn.nomoredomains.xyz";
+// const BASE_URL = "https://api.filmoteka.nkzn.nomoredomains.xyz";
+const BASE_URL = "http://localhost:3001";
 
 const DURATION_SHORT_MOVIES = 40;
 
