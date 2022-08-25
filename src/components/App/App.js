94f45d5b553411347966e4989b75c423c3cd8b00
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-25 09:21:23 +0300

    feat: protected routs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import { Route, Redirect, Switch, useHistory } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
@@ -7,69 +10,185 @@ import Login from "../Login/Login";
 import Register from "../Register/Register";
 import NotFound from "../NotFound/NotFound";
 import InfoTooltipPopup from "../InfoTooltipPopup/InfoTooltipPopup";
-import { Route, Switch, useHistory } from "react-router-dom";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 import Main from "../Main/Main";
 import mainApi from "../../utils/MainApi";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+
 
 function App() {
   const history = useHistory();
-  const [isInfoToolTipOpen, setIsInfoToolTipOpen] = useState(false);
-  const [isRegistered, setIsRegistered] = useState();
-  const [errorMessage, setErrorMessage] = useState("");
+  const [currentUser, setCurrentUser] = useState({});
+  const [isInfoToolTip, setIsInfoToolTip] = useState({
+    isOpen: false,
+    status: true,
+    message: "",
+  });
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isErrMessage, setIsErrMessage] = useState('');
+  const headerPath = ['/', '/movies', '/saved-movies', '/profile'];
+  const footerPath = ['/', '/movies', '/saved-movies'];
 
-  //---Открываем попап сообщения
-  function handleInfoTooltipOpen() {
-    setIsInfoToolTipOpen(true);
+  //---Выводит сообщение об ошибке
+  function informError(err) {
+    setIsInfoToolTip({
+      isOpen: true,
+      status: false,
+      message: err.message,
+    });
   }
 
-
+  //---Регистрация
   function handleRegister({ name, email, password }) {
-    return mainApi
+    mainApi
       .register(name, email, password)
       .then((res) => {
-        if (!res.message) {
-          setIsRegistered(true);
-          handleInfoTooltipOpen();
-        } else {
-          setIsRegistered(false);
-          setErrorMessage(res.message);
-          handleInfoTooltipOpen();
-        }        
-        history.push("/singin");
+        if (res._id) {
+          handleLogin({ email, password });
+        }
+        setIsErrMessage(res.message)
       })
-      .catch((err) => {
-        setIsRegistered(false);
-        setErrorMessage(err);
-        handleInfoTooltipOpen();
-      });
+      .catch((err) => informError(err))
   }
+
+  //---Вход
+  function handleLogin({ email, password }) {
+    mainApi
+      .login(email, password)
+      .then((res) => {        
+        if (res.token) {
+          tokenCheck();
+        }
+        setIsErrMessage(res.message)
+      })
+      .catch((err) => informError(err))
+  }
+
+  useEffect(() => {
+    tokenCheck();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function tokenCheck() {
+    mainApi
+      .getUserInfo()
+      .then((res) => {
+        if (res._id) {
+          setCurrentUser(res);
+          setLoggedIn(true);
+          history.push("/movies");
+        }
+      })
+      .catch((err) => informError(err));
+  }
+
+  //---Редактирование профиля
+  function handleProfile({ name, email }) {
+    mainApi
+      .setUserInfo(name, email)
+      .then(newUser => {
+        setCurrentUser(newUser);
+        setIsInfoToolTip({
+          isOpen: true,
+          status: true,
+          message: "Ваши данные изменены.",
+        });
+      })
+      .catch((err) => informError(err))
+  }
+
+  //---Выход из аккаунта
+  function handleSignOut() {
+    mainApi
+      .logout()
+      .then(() => {
+        setLoggedIn(false);
+        // setIsBurgerMenuOpen(false);
+        localStorage.clear();
+        history.push("/");
+      })
+      .catch((err) => informError(err));
+  }
+
+  //---Закрывает информационный попап
+  function closeInfoTooltipPopup() {
+    setIsInfoToolTip({
+      isOpen: false,
+      status: false,
+      message: "",
+    });
+  }
+
+  //---Закрывает информационный попап по клику на оверлей
+  function closePopupOnOverlay(e) {
+    if (e.target === e.currentTarget) {
+      closeInfoTooltipPopup();
+    }
+  }
+
   return (
-    <div className="page">
-      <Switch>
-        <Route exact path="/">
-          <Main />
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Route exact path={headerPath}>
+          <Header
+            loggedIn={loggedIn}
+          />
         </Route>
-        <Route path="/movies">
-          <Movies />
-        </Route>
-        <Route path="/saved-movies">
-          <SavedMovies />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/signin">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Register handleRegister={handleRegister}/>
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-      <InfoTooltipPopup />
-    </div>
+        <Switch>
+          <Route 
+            exact path="/" 
+            component={Main}
+            loggedIn={loggedIn} 
+          />
+          <ProtectedRoute
+            path="/movies"
+            component={Movies}
+            loggedIn={loggedIn}
+          />
+          <ProtectedRoute
+            path="/saved-movies"
+            component={SavedMovies}
+            loggedIn={loggedIn}
+          />
+          <ProtectedRoute
+            path="/profile"
+            component={Profile}
+            loggedIn={loggedIn}
+            handleSignOut={handleSignOut}
+            isErrMessage={isErrMessage}
+            handleProfile={handleProfile}
+          />
+          <Route path="/signin">
+            <Login
+              handleLogin={handleLogin}
+              isErrMessage={isErrMessage}
+            />
+          </Route>
+          <Route path="/signup">
+            <Register
+              handleRegister={handleRegister}
+              isErrMessage={isErrMessage}
+            />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+          <Route>
+            {loggedIn ? <Redirect to="/" /> : <Redirect to="signin" />}
+          </Route>
+        </Switch>
+        <Route exact path={footerPath}>
+            <Footer />
+          </Route>
+        <InfoTooltipPopup
+          isOpen={isInfoToolTip.isOpen}
+          status={isInfoToolTip.status}
+          onClose={closeInfoTooltipPopup}
+          onCloseOverlay={closePopupOnOverlay}
+          message={isInfoToolTip.message}
+        />
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
