6d7a62141a7452e9db341d4877c23c187adbe4c9
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-25 09:18:32 +0300

    refactor

--- src/components/InfoTooltipPopup/InfoTooltipPopup.js
+++ src/components/InfoTooltipPopup/InfoTooltipPopup.js
@@ -5,23 +5,24 @@ import No from "../../images/InfoTooltipPopup/No.svg";
 function InfoToolTip(props) {
   return (
     <section
-      className={`popup popup_type_status-reg  ${false ? "popup_opened" : ""}`}
-      // onClick={props.onCloseOverlay}
+      className={`popup popup_type_status-reg  ${props.isOpen ? "popup_opened" : ""}`}
+      onClick={props.onCloseOverlay}
     >
       <div className={`popup__container-status`}>
         <button
-          // onClick={props.onClose}
+          onClick={props.onClose}
           className="popup__button-close"
           type="button"
           aria-label="Закрыть окно."
         ></button>
         <img
-          src={true ? Ok : No}
+          src={props.status ? Ok : No}
           className="popup__status-image"
           alt="Статус."
         />
         <h2 className="popup__title">
-          {true ? "Тут будет сообщение!" : `${props.errorMessage}`}
+          {props.message}
+          {/* {props.isRegistered ? "Всё хорошо!" : `${props.errorMessage}`} */}
         </h2>
       </div>
     </section>
