3ebfb9bf94b87fb04885a76abc9032d0a22a613d
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-08-26 09:42:18 +0300

    fix: add go back

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,23 @@
 import "./NotFound.css";
+import { useHistory } from 'react-router-dom';
 
 function NotFound() {
+  const history = useHistory();
+  
+  function goBack() {
+    history.goBack();
+  }
+
   return (
     <>
       <section className="not-found">
         <h1 className="not-found__title">404</h1>
         <p className="not-found__text">Страница не найдена</p>
-        <button className="not-found__back-button" type="button">
+        <button 
+          className="not-found__back-button" 
+          type="button"
+          onClick={goBack}
+        >
           Назад
         </button>
       </section>
