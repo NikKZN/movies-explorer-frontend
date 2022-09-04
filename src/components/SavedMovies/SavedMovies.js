0f4cc1ed006c85f7f07ef6a46a27dee555b6851e
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-09-04 10:03:00 +0300

    fix: bugfix

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -33,6 +33,7 @@ function SavedMovies(props) {
         setMoviesNotFound(false);
         setUserSearchSavedMovies(result);
       }
+      setSearchQuerySavedMovies('');
     }
 
   useEffect(() => {
